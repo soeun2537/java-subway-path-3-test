37de9ebef96dacd100341684624db2a57335235d
soeun2537 <soeun2537@gmail.com>
2024-12-13 20:04:33 +0900

    feat: Application 설정

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,17 @@
 package subway;
 
 import java.util.Scanner;
+import subway.controller.SubwayController;
+import subway.service.SubwayService;
+import subway.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        InputView inputView = InputView.from(scanner);
+
+        SubwayService subwayService = new SubwayService();
+        SubwayController subwayController = new SubwayController(inputView, subwayService);
+        subwayController.run();
     }
 }
